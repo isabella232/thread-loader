ddbb570ed8de6a44c94c248adc3486fdd3ef7a0c
Kevin Burke <kev@inburke.com>
2018-10-05 11:02:04 -0700

    Add dist directory
    It's in the uploaded package but not the source directory, which means
    if you are importing directly from the fork the package fails to load.
    Checking it in will allow us to track changes to it over time.

---
+++ dist/readBuffer.js
@@ -0,0 +1,54 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = readBuffer;
+function readBuffer(pipe, length, callback) {
+  if (length === 0) {
+    callback(null, new Buffer(0));
+    return;
+  }
+  var terminated = false;
+  var remainingLength = length;
+  var buffers = [];
+  var readChunk = function readChunk() {
+    var onChunk = function onChunk(arg) {
+      if (terminated) {
+        return;
+      }
+      var chunk = arg;
+      var overflow = void 0;
+      if (chunk.length > remainingLength) {
+        overflow = chunk.slice(remainingLength);
+        chunk = chunk.slice(0, remainingLength);
+        remainingLength = 0;
+      } else {
+        remainingLength -= chunk.length;
+      }
+      buffers.push(chunk);
+      if (remainingLength === 0) {
+        pipe.pause();
+        pipe.removeListener('data', onChunk);
+        if (overflow) {
+          pipe.unshift(overflow);
+        }
+        terminated = true;
+        callback(null, Buffer.concat(buffers, length));
+      }
+    };
+    var onEnd = function onEnd() {
+      if (terminated) {
+        return;
+      }
+      terminated = true;
+      var err = new Error(`Stream ended ${remainingLength.toString()} bytes prematurely`);
+      err.name = 'EarlyEOFError';
+      callback(err);
+    };
+    pipe.on('data', onChunk);
+    pipe.on('end', onEnd);
+    pipe.resume();
+  };
+  readChunk();
+}
